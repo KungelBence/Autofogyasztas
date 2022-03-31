ce1418af0f6834f52ced05fd7a3e15f97d571575
KungelBence <MagyarKingHun469@gmail.com>
2022-03-31 14:25:11 +0200

    sad

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 
+
+function szamol () {
 let F = document.getElementById('fogyasztás').value;
 let V = document.getElementById('tank').value;
-let UT = document.getElementById('ut').value;
+let U = document.getElementById('ut').value;
 
+let megtehetoUt = V * 100/F
+if(megtehetoUt <Ut ){
+document.getElementById('megoldás').innerHTML = 'Az út meg tehető tankolás nélkül'
+}
+else {
+document.getElementById('megoldás').innerHTML = 'Az út meg tehető tankolás nélkül'
+}
 
-
+}
 
 
 
