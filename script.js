0aa19771edaa9e9569d1158874987180f31215b7
KungelBence <MagyarKingHun469@gmail.com>
2022-04-07 14:23:20 +0200

    Jav próba

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-
+let szamolas = function szamol () {
 function szamol () {
-    let F = document.getElementById('fogyasztás').value;
+    
+    let F = document.getElementById('fogyasztas').value;
     let V = document.getElementById('tank').value;
-    let UT = document.getElementById('ut').value;
-    
-    let megtehetoUT = V * 100 / F;
-    
-    document.getElementById('jel').innerHTML = "Az út megtehető tankolás nélkül"
-    
- if (megtehetoUT < UT) {
-        document.getElementById('megoldas').innerHTML = "Az út során tankolni kell!";
- } else {
-    document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül";
-}
-}
-document.getElementById('szamol').click
+    let Ut = document.getElementById('ut').value;
+
+    let megtehetoUt = V * 100 / F;
+
+
+    if (megtehetoUt < Ut) {
+    document.getElementById('megoldas').innerHTML = "Az út során tankolni kell!"
+    } else {
+    document.getElementById('megoldas').innerHTML = "Az út megtehető tankolás nélkül."
+    }
+}}
+document.getElementById('szamol').addEventListener('click', szamolas);
